5046cf6666ce84b101aaa5018c5e7a31cbf53f89
Mohammad Arabzadeh <a.mohamad7824@gmail.com>
2021-11-14 21:24:19 +0330

    added Node constructors and methods

--- cpp/linkedlist.cpp
+++ cpp/linkedlist.cpp
@@ -1,1 +1,21 @@
 #include "linkedlist.h"
+
+LinkedList::Node::Node()
+    : value {}
+{
+}
+
+LinkedList::Node::Node(double val)
+    : value { val }
+{
+}
+
+double LinkedList::Node::getValue()
+{
+    return value;
+}
+
+void LinkedList::Node::setValue(double val)
+{
+    value = val;
+}
