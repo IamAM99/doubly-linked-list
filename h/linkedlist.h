fd11827ac89ab6bb1c901d423391e16e197f5bbb
Mohammad Arabzadeh <a.mohamad7824@gmail.com>
2021-11-14 21:23:27 +0330

    added ifndef to linkedlist.h

--- h/linkedlist.h
+++ h/linkedlist.h
@@ -1,18 +1,22 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
 class LinkedList {
 public:
     class Node {
     public:
         Node();
-        Node(double );
-        Node *next;
-        Node *previous;
+        Node(double val);
+        Node* next;
+        Node* previous;
         double getValue();
-        void setValue(double);
+        void setValue(double val);
+
     private:
         double value;
     };
     LinkedList();
-    LinkedList(const LinkedList &);
+    LinkedList(const LinkedList&);
     ~LinkedList();
     void push_back(double);
     void push_front(double);
@@ -24,10 +28,14 @@ public:
     void clear();
     void show();
     int getSize();
-    void extend(const LinkedList &);
+    void extend(const LinkedList&);
+
 private:
-    int N{0};
+    int N { 0 };
+
 public:
-    Node *head;
-    Node *tail;
+    Node* head;
+    Node* tail;
 };
+
+#endif
